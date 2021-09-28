b3a5a0f4a4f8a750920b0e49009ae517cbfb2231
Kevin-Rojo <61994525+Kevin-Rojo@users.noreply.github.com>
2021-09-27 21:25:41 -0500

    Prueba 2 API VT

--- Prueba API VT/Prueba_API.py
+++ Prueba API VT/Prueba_API.py
@@ -1,4 +1,4 @@
-#api Key 529aad229b636a3bfab6f6c9a3ecc25dcee6efb3e0c93200d95a4e2e3278af5e
+
 
 from __future__ import print_function
 import os
@@ -6,20 +6,21 @@ import json
 import hashlib
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 
-API_KEY = '529aad229b636a3bfab6f6c9a3ecc25dcee6efb3e0c93200d95a4e2e3278af5e'
-path_file = input("ingrese la ubicacion del archivo a analizar")
+#path_file = input("ingrese la ubicacion del archivo a analizar")
 
-print(os.listdir('../../archivo de prueba/EICAR.com'))
+#print(os.listdir('I:\FCFM\GitHub\PIA_PC\Prueba API VT\API'))
 
+with open('Prueba API VT/APIKEY.txt', "r") as f:
+    API_KEY = f.readline()
 
-with open('I:/FCFM/archivo de prueba/EICAR.com', "rb") as f:
-    file_hash = md5(f.read()).hexdigest()
+with open('../../archivo de prueba/NoVirus.txt', "rb") as f:
+    file_hash = hashlib.md5(f.read()).hexdigest()
 
-EICAR = "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*".encode('utf-8')
-#print(EICAR)
-file_MD5 = hashlib.md5(EICAR).hexdigest()
-input()
 vt = VirusTotalPublicApi(API_KEY)
 
 response = vt.get_file_report(file_hash)
 print(json.dumps(response, sort_keys=False, indent=4))
+
+
+if response['results']['positives'] > (response['results']['total']/2):
+    print("malware")
