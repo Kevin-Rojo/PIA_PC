61e62db896b031de30b2299cca732aae8e3d5a0b
Kevin-Rojo <61994525+Kevin-Rojo@users.noreply.github.com>
2021-11-11 14:57:52 -0600

    123

--- DocumentGatering.py
+++ DocumentGatering.py
@@ -3,55 +3,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import re
 #from urllib import request
-"""
-    Orden para Funcion Manual
 
-    Archivos: [
-        archivo1[
-            URL, NombreDeArchivo, TipoArchivo
-        ],
-        archivo2[
-            URL, NombreDeArchivo, TipoArchivo
-        ], 
-        archivo3[
-            URL, NombreDeArchivo, TipoArchivo
-        ], 
-        archivo4[
-            URL, NombreDeArchivo, TipoArchivo
-        ],  
-    ]
-
-    ListaArchivosTotales: [
-        Archivos1: [
-            archivo1[
-                URL1, NombreDeArchivo, TipoArchivo
-            ],
-            archivo2[
-                URL1, NombreDeArchivo, TipoArchivo
-            ], 
-            archivo3[
-                URL1, NombreDeArchivo, TipoArchivo
-            ], 
-            archivo4[
-                URL1, NombreDeArchivo, TipoArchivo
-            ],  
-        ],
-        Archivos2: [
-            archivo1[
-                URL2, NombreDeArchivo, TipoArchivo
-            ],
-            archivo2[
-                URL2, NombreDeArchivo, TipoArchivo
-            ], 
-            archivo3[
-                URL2, NombreDeArchivo, TipoArchivo
-            ], 
-            archivo4[
-                URL2, NombreDeArchivo, TipoArchivo
-            ],  
-        ]
-    ]
-"""
 
 #################################################################################################################################
 #                           Funcion que permite descargar documento por medio de un url                                         #
@@ -73,63 +25,62 @@ import re
 #       re              patronGroups    Almacena el resultado del analisis de la expresion regular                              #
 #                                                                                                                               #
 #################################################################################################################################
-
-
 def GetWebDcumentsURL(url:str)->list:
-
     #Peticion a la URL y lectura del HTML en un objeto de BeautifulSoup
     html = requests.get(url)
-    soup = bs(html.content, 'html.parser')
-    #Se define le expresion regulrar para la busqueda de archivos conformada por 3 grupos (URL)(nombre del documento(Tipo de archivo))
+    soup = bs(html.content, 'html.parser')#se crea el objeto de bs para ordenar el html al que se le hace la peticion 
 
-    #   (URL)(NombreArchivo(tipo de Archivo))
-    links= soup.find_all(href=re.compile("(.+[a-z0-9]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))"))#etiqueta < href="">
+    #se buscan todas las etiquetas que tengan un  href y que contengan un docuemnto 
+    a= soup.find_all(href=re.compile("(.+[a-z0-9]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))"))#etiqueta < href="">
 
     #   [<a1>['href'="link1"], <a2>['href'="link2"], <a3>['href'="link3"]]  href=ruta del documento
-    lista_Documentos=[]# Aqui se guardan los links para la descarga
-    for document in links:
-        lista_Documentos.append(document['href'])
+    listOfDocs=[]# Aqui se guardan los links para la descarga
+    for document in a:
+        listOfDocs.append(document['href'])# por cada objeto que encontro guarda la ruta done se encuentra el archivo y la ruta se encuentra en el href
+    #arriba se declara la expresion regular 
+
+
     
-    # lista_Documentos=[ruta1, ruta2 , ruta3, ruta1]
+    # listOfDocs=[ruta1, ruta2 , ruta3, ruta1]
     #Limpia documentos Repetidos
-    for documento in lista_Documentos:
-        while lista_Documentos.count(documento)!=1:
-            lista_Documentos.remove(documento)
-    # lista_Documentos=[ruta2 , ruta3, ruta1]
+    for doc in listOfDocs:
+        while listOfDocs.count(doc)!=1:
+            listOfDocs.remove(doc)
+    # listOfDocs=[ruta2 , ruta3, ruta1]
 
 
 
-    Archivos=[] #Almacena la informacion de los documentos descargados [[url,nombre del archivo, tipo del archivo],...]
-    
-    #en este ciclo se copian todos los documentos encntrados en la lista "lista_Documentos" y se guardan en la carpeta DocumentosExtraidos
-    for documento in lista_Documentos:
-        
-        #se declara la expresion regulrar para separar la informacion en 3 grupos:
-        #grupo1="ruta del archivo(link)" grupo2="nombre del archivo con extension" groupo3="extension del archivo"
-        filePatron=re.compile(r"(.+[a-z0-9]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))")
+    Archivos=[]
 
-        #busca el patron dentro de la ruta completa donde se encuentra el documento
-        patronGroups=filePatron.search(documento)
+    #ruta donde se guarda el documento
+    for doc in listOfDocs:
 
-        #nombra la ruta donde se guardara el archivo en este caso es /DocumentosExtraidos/NombreDelArchivo.Extension
+        filePatron=re.compile(r"(.+[a-z0-9|]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))")
+
+        patronGroups=filePatron.search(doc)# Almacena el resultado del analisis de la expresion regular 
+
+        print(doc)
+        print(patronGroups.group(2))
+
         dir="DocumentosExtraidos\\"+patronGroups.group(2) #ruta y nombre del archivo a copiar
 
-        documento_descargado = requests.get(documento, allow_redirects=True)
+        myfile = requests.get(doc, allow_redirects=True)#se guarda el archivo copiado
 
 
-        open(dir, 'wb').write(documento_descargado.content)
+        open(dir, 'wb').write(myfile.content)#abre un documento en blanco y copia el archivo en ese documento
         #[URL, NombreDeArchivo, Tipo]
-        tempList = [documento,patronGroups.group(2),patronGroups.group(3)]
+        tempList = [doc,patronGroups.group(2),patronGroups.group(3)]
+        ##tempDict={'URL':doc,'FileName':patronGroups.group(2),'FileType':patronGroups.group(3)}
+        Archivos.append(tempList)#los datos se guardan en una lista temp. y se guarda en otra lista 
 
-        Archivos.append(tempList)
-
+    print(len(Archivos))
     if len(Archivos)>0:
         return Archivos
     else:
         print("no se han extraido Documentos")
 
 #descarga Archivos de Paginas web desde una lista de URLs que recive por un directorio
-def GetWebDcumentsListURL(urlDir:str)->list:
+def GetWebDcumentsListURL(urlDir:str)->list:#esta funcion recibe un directorio 
 
     # Inicia Abriendo el Archivo txt con la lista de URL a Analizar
     f = open(urlDir, "r")
@@ -140,19 +91,20 @@ def GetWebDcumentsListURL(urlDir:str)->list:
 
         a= soup.find_all(href=re.compile("(.+[a-z0-9]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))"))
 
-        lista_Documentos=[]
+        listOfDocs=[]
         for document in a:
-            lista_Documentos.append(document['href'])
+            listOfDocs.append(document['href'])
 
-        for doc in lista_Documentos:
-            while lista_Documentos.count(doc)!=1:
-                lista_Documentos.remove(doc)
+        for doc in listOfDocs:
+            while listOfDocs.count(doc)!=1:
+                listOfDocs.remove(doc)
 
         Archivos=[]
-        for doc in lista_Documentos:
+        for doc in listOfDocs:
             filePatron=re.compile(r"(.+[a-z0-9]\/)+(.+\.(pdf|docx|doc|PDF|xlsx|xls|ppt|txt|sql))")
             patronGorups=filePatron.search(doc)
             print(doc)
+            print(patronGorups.group(2))
             dir="DocumentosExtraidos\\"+patronGorups.group(2)
             myfile = requests.get(doc, allow_redirects=True)
             open(dir, 'wb').write(myfile.content)
@@ -160,7 +112,7 @@ def GetWebDcumentsListURL(urlDir:str)->list:
             tempList = [doc,patronGorups.group(2),patronGorups.group(3)]
             #BORRAR#    tempDict={'URL':doc,'FileName':patronGorups.group(2),'FileType':patronGorups.group(3)}
             Archivos.append(tempList)
-
+        print(len(Archivos))
         if len(Archivos)>0:
             tempList=[url,Archivos]
             ListaArchivosTotales.append(tempList)
